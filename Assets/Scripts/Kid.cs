746859dc0945fddbc5a5f910a8329aa3648c8aac
Heaq <siyaoh4@uci.edu>
2022-10-21 22:51:17 -0700

    KidsController

--- Assets/Scripts/Kid.cs
+++ Assets/Scripts/Kid.cs
@@ -1,6 +1,16 @@
-namespace DefaultNamespace;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Kid
 {
-    
+    public bool shownUp;
+    public bool sprayed;
+
+    public Kid()
+    {
+        shownUp = false;
+        sprayed = false;
+
+    }
 }
