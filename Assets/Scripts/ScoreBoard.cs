479ec269cf45c506b8497a22f112583cd7835b96
glibertt <sleestef@uci.edu>
2022-10-22 21:18:21 -0700

    more

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class ScoreBoard : MonoBehaviour
 {
+<<<<<<< Updated upstream
     // Start is called before the first frame update
     void Start()
     {
         
+=======
+    static public int candyCounter;
+    static public int happyKidsCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        candyCounter = 100;
+        happyKidsCounter = 0;
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -15,4 +26,16 @@ public class ScoreBoard : MonoBehaviour
     {
         
     }
+<<<<<<< Updated upstream
+=======
+
+    public static int GetCandyCounter()
+    {
+        return candyCounter;
+    }
+    public static void DecreaseCandyCounter(int candyNum)
+    {
+        candyCounter -= candyNum;
+    }
+>>>>>>> Stashed changes
 }
